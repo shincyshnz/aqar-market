a7639b9616c32900be7be95618a6aef80724cdaa
shincy <shincyshnz@gmail.com>
2024-04-21 17:35:09 +0400

    filter updated

--- src/app/properties/page.jsx
+++ src/app/properties/page.jsx
@@ -7,7 +7,7 @@ export default function Page() {
       <div className="font-openSans text-base flex-1 max-w-[25.3rem] min-h-screen shadow-md rounded-[26px] px-11 py-[1.375rem]">
         <p className="mb-6"><span>Filter</span></p>
         {/* Location filter */}
-        <div className="flex flex-col gap-3 mb-[1.3125rem]">
+        <div className="flex flex-col gap-3 mb-21">
           <p className="opacity-[50%]">Location</p>
           <Dropdown
             data={"Location"}
@@ -16,7 +16,7 @@ export default function Page() {
         </div>
 
         {/* All filter */}
-        <div className="flex flex-col gap-3 mb-[1.3125rem]">
+        <div className="flex flex-col gap-3 mb-21">
           <p className="opacity-[50%]">Property Type</p>
           <Dropdown
             data={"All"}
@@ -24,11 +24,40 @@ export default function Page() {
         </div>
 
         {/* Price Range filter */}
-        <div className="flex flex-col gap-3 mb-[1.3125rem]">
+        <div className="flex flex-col gap-3 mb-21">
           <p className="opacity-[50%]">Price Range</p>
           <PriceRangeSlider />
         </div>
 
+        <hr className="mb-21" />
+
+        {/* Size filter */}
+        <div className="flex flex-col gap-3 mb-21">
+          <p className="opacity-[50%]">Size</p>
+          <div className="flex items-center justify-center gap-5">
+            {/* <input type="text" placeholder="Min" /> */}
+            <div className=" w-1/2 flex justify-between items-center border-[1px] border-drop-down-gray rounded-lg py-2 focus:ring-gray-300">
+              <input
+                type="number"
+                class="w-full px-4 py-2 focus:outline-none remove-arrow"
+                aria-controls="none"
+                placeholder="Min"
+                id="sqftInput"
+              />
+              <label className="opacity-[50%] px-4">sqft</label>
+            </div>
+            <div className="w-1/2 text-black flex justify-between items-center border-[1px] border-drop-down-gray rounded-lg py-2 focus:ring-gray-300">
+              <input
+                type="number"
+                class="w-full px-4 py-2 focus:outline-none remove-arrow"
+                aria-controls="none"
+                placeholder="Max"
+                id="sqftInput"
+              />
+              <label className="opacity-[50%] px-4">sqft</label>
+            </div>
+          </div>
+        </div>
 
       </div>
 
--- src/components/PriceRangeSlider/PriceRangeSlider.jsx
+++ src/components/PriceRangeSlider/PriceRangeSlider.jsx
@@ -5,7 +5,7 @@ import 'react-range-slider-input/dist/style.css';
 
 const PriceRangeSlider = () => {
   const [minMaxPrice, setMinMaxPrice] = useState([100, 1000]);
-  const [priceRange, setPriceRange] = useState([100, 500]);
+  const [priceRange, setPriceRange] = useState([200, 700]);
 
   const handlePriceChange = (value) => {
     // value is the new array with min and max values
@@ -16,7 +16,7 @@ const PriceRangeSlider = () => {
   return (
       <div className="relative mb-6">
     <label htmlFor="labels-range-input" className="sr-only">Labels range</label>
-    <RangeSlider id="range-slider-blue" min={minMaxPrice[0]} max={minMaxPrice[1]} step={1} value={priceRange} onInput={handlePriceChange} />
+    <RangeSlider id="range-slider-blue" className="mb-[13px]" min={minMaxPrice[0]} max={minMaxPrice[1]} step={1} value={priceRange} onInput={handlePriceChange} />
     
     <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">{`AED ${minMaxPrice[0]}`}</span>
     {/* <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">{priceRange[0]}</span> */}
